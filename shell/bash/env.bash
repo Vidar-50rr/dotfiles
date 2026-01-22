0f7212f5efb516be62db48ba9786b06621146291
avenger-wsl <avenger-wsl@jokerwin.com>
2026-01-22 02:03:34 +0100

    add dotfiles

---
+++ shell/bash/env.bash
@@ -0,0 +1,10 @@
+# Shell behavior
+
+HISTCONTROL=ignoreboth
+shopt -s histappend
+HISTSIZE=1000
+HISTFILESIZE=2000
+shopt -s checkwinsize
+
+# Case-insensitive tab completion
+bind 'set completion-ignore-case on'
