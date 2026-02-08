417f21c30fe933fee6dd2551e2873555a70f0af4
avenger-wsl <avenger-wsl@jokerwin.com>
2026-02-08 16:30:46 +0100

    Configure Docusaurus base URL for GitHub Pages

--- docs-site/docusaurus.config.js
+++ docs-site/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'http://localhost',
+  url: 'https://Vidar-50rr.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/dotfiles/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
